7ed3c52636abeb4625d7cabc1145e486abf79291
aliya-rahamani <65673692+aliya-rahamani@users.noreply.github.com>
2020-08-18 14:42:12 +0530

    Add files via upload

---
+++ rock_paper_scissor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+int main ()
+
+{
+
+  srand(time(NULL));
+
+ int computerChoice = rand() % 3 + 1;
+   int userChoice=0;
+   int score=0;
+   int score2=0;
+std::cout << "====================\n";
+std::cout << "rock paper scissors!\n";
+std::cout << "====================\n";
+
+std::cout << "1 :rock\n";
+std::cout << "2 :paper\n";
+std::cout << "3 :scissor\n";
+
+std::cout << "shoot! ";
+
+  std::cin >> userChoice;
+
+ std::cout <<   rand() % 3 + 1 << "\n";
+
+
+
+if(computerChoice == 1 && userChoice == 1) {
+    std::cout << "It was a tie!" << std::endl;
+} else if (computerChoice == 1 && userChoice == 3) {
+    std::cout << "The computer won! Better luck next time!" << std::endl;
+} else if (computerChoice == 2 && userChoice == 2) {
+    std::cout << "It was a tie!" << std::endl;
+} else if (computerChoice == 1 && userChoice == 1) {
+    std::cout << "The computer won! Better luck next time!" << std::endl;
+} else if (computerChoice == 3 && userChoice == 3) {
+    std::cout << "It was a tie!" << std::endl;
+} else if (computerChoice == 3 && userChoice == 2) {
+    std::cout << "The computer won! Better luck next time!" << std::endl;
+} else {
+    std::cout << "Congrats! You won!" << std::endl;
+}
+
+return 0;
+}
